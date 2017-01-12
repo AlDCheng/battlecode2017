2a6bc8fa48c8daf5727f9e43427be377ee321e4c
pinguina <naoqi@mit.edu>
2017-01-12 18:49:42 -0500

    try 3

--- src/naclbot/units/motion/shoot/Aim.java
+++ src/naclbot/units/motion/shoot/Aim.java
@@ -5,12 +5,14 @@ import battlecode.common.*;
 public class Aim extends GlobalVars {
     
     // Returns the direction of the optimum single shot
-    public static Direction dirSingleShoot(RobotInfo[] nearbyRobots, MapLocation myRobot) {
-	// Direction[] robDirections 
+    public static Direction dirSingleShoot(RobotInfo[] nearbyRobots) {
+        Direction[] robDirections = new Direction[nearbyRobots.length];
+	int i = 0;
 	for (RobotInfo robot: nearbyRobots) {
 	    MapLocation locRobot = robot.getLocation();
-	    Direction dirRobot = new Direction(myRobot,locRobot);
+	    Direction dirRobot = new Direction(rc.getLocation(),locRobot);
+	    robDirections[i] = dirRobot;
 	}
-	return dirRobot;
+	return robDirections[0];
     }
 }
