5e2c024b3efdde9776b5c97ec2ae5f03408eb0d3
Mutae3912 <jueunlee@mit.edu>
2017-01-11 23:56:19 -0500

    jueun push pt.2

--- src/naclbot/units/motion/search/TreeSearch.java
+++ src/naclbot/units/motion/search/TreeSearch.java
@@ -1,1 +1,23 @@
-Test
+// General move function from example
+package naclbot;
+import battlecode.common.*;
+
+public class TreeSearch extends GlobalVars {
+	
+	static int countNearbyTrees() {
+		return rc.senseNearbyTrees().length;
+		
+	}
+	/*
+	static TreeInfo[] getNearbyViableTrees() {
+		ArrayList<MapLocation> viableList = new ArrayList<MapLocation>();
+		TreeInfo[] treeList = rc.senseNearbyTrees();
+		for (TreeInfo tree: treeList) {
+			if (tree.containedBullets > 0) {
+				viableList.add(tree.location);
+			}
+		}
+		return viableList;
+	}
+	*/
+}
