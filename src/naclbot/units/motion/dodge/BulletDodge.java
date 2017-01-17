dbfade5c17daa0ff1a424e5ed3b5b1fa9e3497ff
pinguina <naoqi@mit.edu>
2017-01-16 21:32:41 -0500

    hihihihihihi

--- src/naclbot/units/motion/dodge/BulletDodge.java
+++ src/naclbot/units/motion/dodge/BulletDodge.java
@@ -6,24 +6,19 @@ import naclbot.variables.GlobalVars;
 public class BulletDodge extends GlobalVars {
 
     public static Direction whereToDodge(BulletInfo[] bullets) {
-	DodgeType closestBullet = new DodgeType(bullets[0]);
-	float closestBulletDistance = 10000;
-	    
+		    
 	for (BulletInfo bullet: bullets) {
 	    
 	    DodgeType dodgeInfo = new DodgeType(bullet);
 	    boolean willCollide = dodgeInfo.willCollide();
 	    float distance = dodgeInfo.getDistToRobot();
 	    	    
-	    if (willCollide == true && distance < closestBulletDistance) {
-		closestBulletDistance = distance;
-		closestBullet = dodgeInfo;
+	    if (willCollide == true) {
+		Direction dir = dodgeInfo.getDirectionToMove();
+		return dir;
 	    }	    
 	}
-	if (closestBulletDistance != 10000) {
-	    Direction dir = closestBullet.getDirectionToMove();
-	    return dir;
-	}
+	
 	Direction dir = new Direction(-1);
 	return dir;
     }
