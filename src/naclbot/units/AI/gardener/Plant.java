a3633aef41dbd9c486c640e2ff56356a0eae9c8c
Alan Cheng <adcheng@mit.edu>
2017-01-17 19:32:45 -0500

    hotfixes for joon

--- src/naclbot/units/AI/gardener/Plant.java
+++ src/naclbot/units/AI/gardener/Plant.java
@@ -8,27 +8,36 @@ import java.util.ArrayList;
 public class Plant extends GlobalVars {
 	
 	public static void plantToLocation(MapLocation potentialPlantLoc) {
-		//makes gardeners walk to left of potentialPlantLoc and plant a tree there
-		float distanceFromTree = 1 + GameConstants.BULLET_TREE_RADIUS;
-		MapLocation gardenerLocation = potentialPlantLoc.add(Direction.getWest(),distanceFromTree);
-		Direction dirToPlantLocation = rc.getLocation().directionTo(gardenerLocation);
-		if (rc.getLocation() == gardenerLocation) {
-			rc.plantTree(Direction.getEast());
-		}
-		else {
-			Move.tryMove(dirToPlantLocation);
+		try {
+			//makes gardeners walk to left of potentialPlantLoc and plant a tree there
+			float distanceFromTree = 1 + GameConstants.BULLET_TREE_RADIUS;
+			MapLocation gardenerLocation = potentialPlantLoc.add(Direction.getWest(),distanceFromTree);
+			Direction dirToPlantLocation = rc.getLocation().directionTo(gardenerLocation);
+			if (rc.getLocation() == gardenerLocation) {
+				rc.plantTree(Direction.getEast());
+			}
+			else {
+				Move.tryMove(dirToPlantLocation);
+			}
+		} catch(GameActionException e) {
+			e.printStackTrace();
 		}
 	}
 	
 	public static ArrayList<MapLocation> possibleNeighborLocations(MapLocation treeLocation, float radius) {
 		ArrayList<MapLocation> possibleLocations = new ArrayList<MapLocation>();
 		ArrayList<MapLocation> existingTrees = TreeSearch.getNearbyTrees();
-		Direction[] directionList = {Direction.getNorth(),Direction.getEast(),Direction.getSouth(),Direction.getWest()};
-		for (int i=0; i<4; i++) {
-			MapLocation possibleLoc = treeLocation.add(directionList[i], radius);
-			if (!existingTrees.contains(possibleLoc)) {
-			possibleLocations.add(possibleLoc); 
+		
+		try {
+			Direction[] directionList = {Direction.getNorth(),Direction.getEast(),Direction.getSouth(),Direction.getWest()};
+			for (int i=0; i<4; i++) {
+				MapLocation possibleLoc = treeLocation.add(directionList[i], radius);
+				if (!existingTrees.contains(possibleLoc)) {
+				possibleLocations.add(possibleLoc); 
+				}
 			}
+		} catch(Exception e) {
+			e.printStackTrace();
 		}
 		return possibleLocations;
 		
