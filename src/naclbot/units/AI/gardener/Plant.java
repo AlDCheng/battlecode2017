07c839a45ecb0aca32eeeb75db46cfab87d94253
Mutae3912 <jueunlee@mit.edu>
2017-01-18 14:12:24 -0500

    ad;fj

--- src/naclbot/units/AI/gardener/Plant.java
+++ src/naclbot/units/AI/gardener/Plant.java
@@ -7,31 +7,47 @@ import java.util.ArrayList;
 
 public class Plant extends GlobalVars {
 	
-	public static void plantToLocation(MapLocation potentialPlantLoc) {
+	public static void plantToLocation(MapLocation potentialPlantLoc, Direction dirToPlant) {
 		try {
+			System.out.println(rc.getID() + " " + 2);
 			//makes gardeners walk to left of potentialPlantLoc and plant a tree there
 			float distanceFromTree = 1 + GameConstants.BULLET_TREE_RADIUS; //distance between center of gardener and center of tree being planted
-			MapLocation gardenerLocation = potentialPlantLoc.add(Direction.getWest(),distanceFromTree);
+			MapLocation gardenerLocation = potentialPlantLoc.subtract(dirToPlant,distanceFromTree);
 			Direction dirToPlantLocation = rc.getLocation().directionTo(gardenerLocation); 
-			if (rc.getLocation() == gardenerLocation) {
-				//at location so can plant 
-				rc.plantTree(Direction.getEast());
-			}
-			else {
+			System.out.println("isbuildready: " + rc.isBuildReady());
+			while (!rc.getLocation().equals(gardenerLocation) || !rc.isBuildReady() || !rc.canPlantTree(dirToPlant)) {
 				//not at location so move toward the gardenerLocation
-				Move.tryMove(dirToPlantLocation);
+				System.out.println(rc.getLocation().y + " " + gardenerLocation.y);
+				float distanceToLoc = rc.getLocation().distanceTo(gardenerLocation);
+				if (distanceToLoc <= 1 && rc.canMove(dirToPlantLocation,distanceToLoc)) {
+					rc.move(dirToPlantLocation,distanceToLoc);
+				} else {
+					try {
+						if (rc.canMove(dirToPlantLocation)) {
+							Move.tryMove(dirToPlantLocation);
+						}
+					} catch(GameActionException e) {
+						e.printStackTrace();
+					}
+				}
+				Clock.yield();
+				
 			}
+			System.out.println("teambullets:" + rc.getTeamBullets());
+			rc.plantTree(dirToPlant);
 		} catch(GameActionException e) {
 			e.printStackTrace();
 		}
+
 	}
 	
-	public static ArrayList<MapLocation> possibleNeighborLocations(MapLocation treeLocation, float radius) {
+	public static MapLocation[] possibleNeighborLocations(MapLocation treeLocation) {
 		// generates MapLocations where trees can be planted to make an organized grid
-		ArrayList<MapLocation> possibleLocations = new ArrayList<MapLocation>();
+		MapLocation[] possibleLocations = new MapLocation[3];
 		ArrayList<MapLocation> existingTrees = TreeSearch.getNearbyTrees();
-		
+		float spacing = (float) 1/10;
 		try {
+			/*
 			Direction[] directionList = {Direction.getNorth(),Direction.getEast(),Direction.getSouth(),Direction.getWest()};
 			for (int i=0; i<4; i++) {
 				MapLocation possibleLoc = treeLocation.add(directionList[i], radius); //generates possible locations we can plant a tree
@@ -39,13 +55,49 @@ public class Plant extends GlobalVars {
 				//if possible location is not already occupied by a tree
 				possibleLocations.add(possibleLoc); 
 				}
-			}
+			*/
+			possibleLocations[0] = treeLocation.add(Direction.getEast(),2*GameConstants.BULLET_TREE_RADIUS+spacing);
+			possibleLocations[1] = possibleLocations[0].add(Direction.getSouth(),2*GameConstants.BULLET_TREE_RADIUS+spacing);
+			possibleLocations[2] = treeLocation.add(Direction.getSouth(),2*GameConstants.BULLET_TREE_RADIUS+spacing);
+
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 		return possibleLocations;
 		
 		
+	}
+	
+	public static void plantCluster() throws GameActionException {
+		MapLocation[] possibleTreeLocations;
+		if (rc.canPlantTree(Direction.getSouth())) {
+			System.out.println(rc.getID() + " " + 1);
+			rc.plantTree(Direction.getSouth());
+			possibleTreeLocations = Plant.possibleNeighborLocations(rc.getLocation().add(Direction.getSouth(),1+GameConstants.BULLET_TREE_RADIUS));
+			Plant.plantToLocation(possibleTreeLocations[0],Direction.getSouth());
+			Plant.plantToLocation(possibleTreeLocations[1],Direction.getNorth());
+			Plant.plantToLocation(possibleTreeLocations[2],Direction.getNorth());
+		}
+	}
+	
+	public static boolean nearbyGardeners(float radius) {
+		RobotInfo[] nearbyRobots = rc.senseNearbyRobots(radius);
+		for (RobotInfo robot: nearbyRobots) {
+			if (robot.getType() == RobotType.GARDENER) {
+				return true;
+			}
+		}
+		return false;
+	}
+	public static boolean nearbyArchons(float radius) {
+		RobotInfo[] nearbyRobots = rc.senseNearbyRobots(radius);
+		for (RobotInfo robot: nearbyRobots) {
+			if (robot.getType() == RobotType.ARCHON) {
+				return true;
+			}
+		}
+		return false;
 	}
 		
 }
